8f3b2ed6f37f98636851ed16a1b897d0b08150bb
LekoArts <lekoarts@gmail.com>
2021-05-12 09:23:49 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  siteMetadata: {
+    title: "issue-31380",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
